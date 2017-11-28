cb89548d26fdf0a0b155ae9c83e051e7d9c3b280
huangguang <huangguang@anchol.com>
2017-11-28 10:52:28 +0800

    Initial commit

---
+++ src/main/java/com/huangguang/DemoInterfaceApplication.java
@@ -0,0 +1,12 @@
+package com.huangguang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInterfaceApplication.class, args);
+	}
+}
